324d7fb93dfb7607ba8bcd43295b243bd635f8f0
Oliveira Hernandes <HERNANDES.OLIVEIRA@prysmiangroup.com>
2022-10-17 18:11:35 -0300

    Removendo Projeto

--- ProvaNetCore/ProvaNetCore/Data/Dtos/Conta/ReadContaDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ProvaNetCore.Data.Dtos.Conta
-{
-    public class ReadContaDto
-    {
-        public int Id { get; set; }
-        public string Nome { get; set; }
-        public string Descricao { get; set; }
-    }
-}
