aa9aa7d29dbd1a7723665ebae0147d43f079b031
Oliveira Hernandes <HERNANDES.OLIVEIRA@prysmiangroup.com>
2022-10-17 18:06:36 -0300

    Inclusão do Porjeto ProvaNetCore

---
+++ ProvaNetCore/ProvaNetCore/Profiles/ContaProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using ProvaNetCore.Data.Dtos.Conta;
+using ProvaNetCore.Models;
+
+namespace ProvaNetCore.Profiles
+{
+    public class ContaProfile : Profile
+    {
+        public ContaProfile()
+        {
+            CreateMap<CreateContaDto, Conta>();
+            CreateMap<UpdateContaDto, Conta>();
+            CreateMap<Conta, ReadContaDto>();
+        }
+    }
+}
---
+++ ProvaNetCore/ProvaNetCore/Services/RestClientService.cs
@@ -0,0 +1,21 @@
+﻿using RestSharp;
+
+namespace ProvaNetCore.Services
+{
+    public class RestClientService
+    {
+        public async Task<string> ExecuteGetRequest(string baseUri, string resource)
+        {
+            RestClient client = new RestClient(baseUri);
+            var request = new RestRequest(resource);
+            var response = await client.ExecuteGetAsync(request);
+            
+            if(response.IsSuccessful)
+            {
+                return response.Content.ToString();
+            }
+
+            return null;
+        }
+    }
+}
